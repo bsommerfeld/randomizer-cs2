a9819f001f9056c7656f8deb0bb45332a9133f4e
Benjamin Sommerfeld <bsommerfeld@users.noreply.github.com>
2025-06-05 03:48:02 +0200

    feat: add interval listeners to sync properties and ensure valid min/max values

--- randomizer-desktop/randomizer/src/main/java/de/bsommerfeld/randomizer/ui/view/viewmodel/settings/ActionSettingsViewModel.java
+++ randomizer-desktop/randomizer/src/main/java/de/bsommerfeld/randomizer/ui/view/viewmodel/settings/ActionSettingsViewModel.java
@@ -6,6 +6,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
 import lombok.Getter;
 
 @Getter
@@ -15,25 +16,49 @@ public class ActionSettingsViewModel {
   private final IntegerProperty minIntervalProperty = new SimpleIntegerProperty();
   private final IntegerProperty maxIntervalProperty = new SimpleIntegerProperty();
 
+  private final ChangeListener<Number> minIntervalUpdateListener = (_, _, _) -> applyInterval();
+  private final ChangeListener<Number> maxIntervalUpdateListener = (_, _, _) -> applyInterval();
+
   public ActionSettingsViewModel() {
     setupActionInFocusListener();
   }
 
-  public void applyInterval() {
+  private void applyInterval() {
     Action currentAction = actionInFocusProperty.get();
     if (currentAction != null) {
       Interval interval = currentAction.getInterval();
-      interval.setMin(minIntervalProperty.get());
-      interval.setMax(maxIntervalProperty.get());
+      int newMin = minIntervalProperty.get();
+      int newMax = maxIntervalProperty.get();
+
+      if (newMin > newMax) {
+        newMax = newMin + 1;
+      }
+
+      interval.setMin(newMin);
+      interval.setMax(newMax);
     }
+  }
+
+  private void addIntervalUpdateListeners() {
+    minIntervalProperty.addListener(minIntervalUpdateListener);
+    maxIntervalProperty.addListener(maxIntervalUpdateListener);
+  }
+
+  private void removeIntervalUpdateListeners() {
+    minIntervalProperty.removeListener(minIntervalUpdateListener);
+    maxIntervalProperty.removeListener(maxIntervalUpdateListener);
   }
 
   private void setupActionInFocusListener() {
     actionInFocusProperty.addListener(
-        (_, _, newAction) -> {
+        (obs, oldAction, newAction) -> {
+          removeIntervalUpdateListeners();
+
           if (newAction != null) {
             minIntervalProperty.set(newAction.getInterval().getMin());
             maxIntervalProperty.set(newAction.getInterval().getMax());
+
+            addIntervalUpdateListeners();
           }
         });
   }
