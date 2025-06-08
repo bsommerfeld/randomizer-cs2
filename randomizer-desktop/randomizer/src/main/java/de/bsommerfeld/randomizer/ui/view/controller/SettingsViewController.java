02f86ce7c216ee7267b75e478951131f03b9faf6
Kjell Witzurke <piepel.kumpel@gmail.com>
2025-06-09 00:21:10 +0200

    design: add Machine Learning settings section to settings-view with new controller, FXML, and styles adjustments

--- randomizer-desktop/randomizer/src/main/java/de/bsommerfeld/randomizer/ui/view/controller/SettingsViewController.java
+++ randomizer-desktop/randomizer/src/main/java/de/bsommerfeld/randomizer/ui/view/controller/SettingsViewController.java
@@ -4,9 +4,11 @@ import com.google.inject.Inject;
 import de.bsommerfeld.randomizer.ui.view.View;
 import de.bsommerfeld.randomizer.ui.view.ViewProvider;
 import de.bsommerfeld.randomizer.ui.view.controller.settings.GeneralSettingsController;
+import de.bsommerfeld.randomizer.ui.view.controller.settings.MachineLearningSettingsController;
 import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
 
 @View
 public class SettingsViewController {
@@ -14,6 +16,7 @@ public class SettingsViewController {
   private final ViewProvider viewProvider;
 
   @FXML private ToggleButton generalToggleButton;
+  @FXML private ToggleButton machineLearningToggleButton;
   @FXML private GridPane contentPane;
 
   @Inject
@@ -23,9 +26,46 @@ public class SettingsViewController {
 
   @FXML
   private void initialize() {
+    generalToggleButton.setOnAction(this::onGeneralToggleButtonAction);
+    machineLearningToggleButton.setOnAction(this::onMachineLearningToggleButtonAction);
+
     generalToggleButton.setSelected(true);
+    loadGeneralSettingsView();
+  }
+
+  @FXML
+  private void onGeneralToggleButtonAction(ActionEvent event) {
+    if (generalToggleButton.isSelected()) {
+      machineLearningToggleButton.setSelected(false);
+      loadGeneralSettingsView();
+    } else {
+      if (!machineLearningToggleButton.isSelected()) {
+        generalToggleButton.setSelected(true);
+      }
+    }
+  }
+
+  @FXML
+  private void onMachineLearningToggleButtonAction(ActionEvent event) {
+    if (machineLearningToggleButton.isSelected()) {
+      generalToggleButton.setSelected(false);
+      loadMachineLearningView();
+    } else {
+      if (!generalToggleButton.isSelected()) {
+        machineLearningToggleButton.setSelected(true);
+      }
+    }
+  }
+
+  private void loadGeneralSettingsView() {
     contentPane
         .getChildren()
         .setAll(viewProvider.requestView(GeneralSettingsController.class).parent());
+  }
+
+  private void loadMachineLearningView() {
+    contentPane
+        .getChildren()
+        .setAll(viewProvider.requestView(MachineLearningSettingsController.class).parent());
   }
 }
---
+++ randomizer-desktop/randomizer/src/main/java/de/bsommerfeld/randomizer/ui/view/controller/settings/MachineLearningSettingsController.java
@@ -0,0 +1,19 @@
+package de.bsommerfeld.randomizer.ui.view.controller.settings;
+
+import com.google.inject.Inject;
+import de.bsommerfeld.randomizer.ui.view.View;
+import javafx.fxml.FXML;
+
+@View
+public class MachineLearningSettingsController {
+
+  @Inject
+  public MachineLearningSettingsController() {
+    // Empty constructor
+  }
+
+  @FXML
+  private void initialize() {
+    // Initialize the controller
+  }
+}
